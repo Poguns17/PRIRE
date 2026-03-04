0043a6fc319dd458754561779725cda8185c3163
poguns_ <praiseoguntibeju@gmail.com>
2026-03-04 15:22:59 +0000

    player gravity

--- PRIRÉ/Assets/Scripts/Player/PlayerMovement.cs
+++ PRIRÉ/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 6f;
     public float mouseSensitivity = 2f;
+    public float gravity = -9.81f;
 
     private CharacterController controller;
     private Vector3 velocity;
@@ -34,6 +35,13 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // Gravity
+        if (controller.isGrounded && velocity.y < 0)
+        velocity.y = -2f;
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
     void HandleMouseLook()
